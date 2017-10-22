aa100ba13602d8b379fa1556b01985dc15a1c1a9
jeffersonMaxmiliano <32546685+jeffersonMaxmiliano@users.noreply.github.com>
2017-10-22 00:19:20 -0200

    Update App.py

--- App.py
+++ App.py
@@ -5,7 +5,7 @@ def main (args=[]):
     def __add__():
         p1 = Point(2, 2)
         p2 = Point(3, 4)
-        return ("Novo ponto x: "+str(p1.x + p2.x) +" Novo ponto x: " +str(p1.y + p2.y))
+        return ("Novo ponto x: "+str(p1.x + p2.x) +" Novo ponto y: " +str(p1.y + p2.y))
 
     def __str__():
         resultado = str(__add__())
